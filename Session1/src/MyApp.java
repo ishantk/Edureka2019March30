5f882e976339578d18b8286202d6e17bab0f1da5
Ishant Kumar <er.ishant@gmail.com>
2019-03-30 09:55:21 +0530

    Session1 - Introduction to Java

---
+++ Session1/src/MyApp.java
@@ -0,0 +1,18 @@
+// MyApp is a class 
+public class MyApp {
+	// main is a method in which we write our code instructions
+	// whatever we write in main, shall be executed by JVM
+	// Execution of code instructions happens sequentially
+	public static void main(String[] args) {
+		// Lets Code 
+		System.out.println("Hello Everyone !!"); // To Print Something on Console
+		System.out.println("Search the Candle rather than cursing the Darkness !!");
+	}
+
+}
+
+class One {
+	
+}
+
+// if we create n-number of classes in our source file, we shall have n-number of .class files in the bin directory
